37860e301584afe6907a872ae71b6d1e18783a0a
David Trompa <dacardo4@gmail.com>
2024-03-29 22:54:30 -0500

    pruebas con rutas desde SPA root OK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { PrimerComponenteComponent } from './components/primer-componente/primer-componente.component';
+import { SegundoComponenteComponent } from './components/segundo-componente/segundo-componente.component';
 
 export const routes: Routes = [
+    { path: 'davidTest/primer', component: PrimerComponenteComponent },
+    { path: 'davidTest/segundo', component: SegundoComponenteComponent },
     { path: '**', component: EmptyRouteComponent },
 ];
